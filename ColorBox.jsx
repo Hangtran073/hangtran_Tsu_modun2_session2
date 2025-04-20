c4e2f9945ef97e553d87d4d6c445c3f263f53482
Hangtran073 <hangt6465@gmail.com>
2025-04-21 00:12:04 +0900

    upload session 02 bài 3

---
+++ ColorBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../main.css";
+
+export default function ColorBox() {
+  return (
+    <div className="parent_box">
+      <div className="child box1">Box</div>
+      <div className="child box2 ">Box</div>
+      <div className="child box3">Box</div>
+    </div>
+  );
+}

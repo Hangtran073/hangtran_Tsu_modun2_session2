8d52d7031b0c1557a727bc72a89bac1cf954607b
Hangtran073 <hangt6465@gmail.com>
2025-04-21 00:15:07 +0900

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import ListCourse from './ListCourse'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ListCourse from './Component_session02/ListCourse'
+import Calculation from './Component_session02/Calculation'
+import UserInfo from './Component_session02/UserInfo'
+import ColorBox from './Component_session02/ColorBox'
+import UserList from './Component_session02/UserList'
+import TodoListIndex from './Component_session02/TodoListIndex'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-   <ListCourse/>
+    <>
+   {/*  <ListCourse/> */}
+   {/* <Calculation/> */}
+   {/* <UserInfo/> */}
+  {/*  <ColorBox/> */}
+  {/* <UserList/> */}
+  <TodoListIndex/>
+
+
+    </>
   )
 }
+
+export default App
